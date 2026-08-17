8c3cd535cb1fe169d6f82f0342c76f908ac0b961
ardrasiva <ardrasivaprasad17@gmail.com>
2026-08-17 15:12:33 +0530

    day 2 homework done

---
+++ Java/bill.java
@@ -0,0 +1,9 @@
+public class bill {
+    public static void main(String[] args) {
+        String ItemName = "Soap";
+        int Quantity = 4;
+        double UnitPrice = 18.75d;
+        double total = UnitPrice * Quantity;
+        System.out.println("Item Name : "+ItemName+"\nQuantity : "+Quantity+"\nUnit Price : "+UnitPrice+"\nTotal Bill : "+total);
+    }
+}
