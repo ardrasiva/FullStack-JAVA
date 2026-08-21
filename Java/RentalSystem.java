306b9e6462c117ce3fa442147cf6a9e1e8148acf
ardrasiva <ardrasivaprasad17@gmail.com>
2026-08-21 15:15:30 +0530

    day 8 home work done

---
+++ Java/RentalSystem.java
@@ -0,0 +1,41 @@
+interface Vehicle{
+    public void start();
+    public void stop();
+}
+interface FuelBased{
+    public void refuel();
+}
+class Car implements Vehicle,FuelBased{
+    public void start(){
+        System.out.println("Car engine started.");
+    }
+    public void stop(){
+        System.out.println("Car engine stopped.");
+    }
+    public void refuel(){
+        System.out.println("Car is refueling at the station.");
+    }
+}
+
+class ElectricScooter implements Vehicle{
+    public void start(){
+        System.out.println("Electric scooter powered on.");
+    }
+    public void stop(){
+        System.out.println("Electric scooter powered off.\n");
+    }
+}
+public class RentalSystem {
+    public static void main(String[] args) {
+        Car c = new Car();
+        ElectricScooter es = new ElectricScooter();
+        System.out.println("\n--Properties of Car class--");
+        c.start();
+        c.refuel();
+        c.stop();
+        System.out.println("\n--Properties of ElectricScooter class--");
+        es.start();
+        es.stop();
+
+    }
+}
